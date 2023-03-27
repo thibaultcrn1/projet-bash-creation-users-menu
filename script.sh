aebc3b9a5af1e8e303b5a1d865cc9faf4229c4f7
thibaultcrn1 <tcarron6@outlook.com>
2023-03-27 16:11:23 +0200

    I

--- script.sh
+++ script.sh
@@ -8,11 +8,11 @@ do
     mdp=$(echo "${dateNaissance:0:4}$prenom" | tr '[:upper:]' '[:lower:]')
     groupe=$(echo "${profession}" | tr '[:upper:]' '[:lower:]')
 
-    sudo groupadd $groupe
+    sudo groupadd "$groupe"
     echo "Le groupe $groupe est créé"
     sudo useradd -m -s /bin/bash -G "$groupe" -c "$prenom $nom" -p "$(openssl passwd -1 "$mdp")" "$utilisateur"
     echo "L'utilisateur $utilisateur est créé"
-    sudo chage -d 0 $utilisateur
+    sudo chage -d 0 "$utilisateur"
  
 done < "./bd_util.csv"
 
